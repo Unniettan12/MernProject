09b5b5576b242bd2c4b4acb2fb9e85a86481f902
Unniettan <harikrishnanrk00@gmail.com>
2026-04-30 17:34:23 +0530

    change to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": import.meta.env.VITE_API_URL,
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
     },
-  },
-});
+  });
+};
